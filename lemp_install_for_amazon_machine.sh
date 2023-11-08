c1dcee3cfae059d47b73bb52dd585293bae475e6
Surajkamble432 <140583784+kamblesuraj432@users.noreply.github.com>
2023-11-08 18:30:58 +0530

    Update lemp_install_for_amazon_machine.sh

--- lemp_install_for_amazon_machine.sh
+++ lemp_install_for_amazon_machine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install nginx mysql-server php -y
+sudo yum install nginx mariadb105-server php -y
 sudo service nginx start
 sudo service mysql start
 sudo service php-fpm start
